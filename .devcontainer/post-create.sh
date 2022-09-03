d237bd4c447d30ff39cd184d41262bb15b281203
Malte Dehling <mdehling@gmail.com>
2022-09-03 20:08:02 +0000

    Fix conda init

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,14 +1,15 @@
-#!/bin/bash
+#!/bin/bash -i
 
 echo "::step::Activating & updating conda environment."
 conda init bash
-source ~/.bashrc
-conda activate ldm
-conda env update -f environment.yaml
+. ~/.bashrc
 
-if [ "x$CONDA_DEFAULT_ENV" != "xldm"]; then
-    echo "export CONDA_DEFAULT_ENV=ldm" >> ~/.bashrc
+if [ "x$CONDA_DEFAULT_ENV" != "xldm" ]; then
+    echo "conda activate ldm" >> ~/.bashrc
+    conda activate ldm
 fi
+
+conda env update -f environment.yaml
 
 echo "::step::Downloading stable-diffusion model."
 bash scripts/download_sd-v1-4.sh
