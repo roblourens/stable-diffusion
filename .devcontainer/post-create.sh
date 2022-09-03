b8573478fee453045d621e19492dc70c561e1005
Malte Dehling <mdehling@gmail.com>
2022-09-03 19:48:36 +0000

    Set CONDA_DEFAULT_ENV

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -6,5 +6,9 @@ source ~/.bashrc
 conda activate ldm
 conda env update -f environment.yaml
 
+if [ "x$CONDA_DEFAULT_ENV" != "xldm"]; then
+    echo "export CONDA_DEFAULT_ENV=ldm" >> ~/.bashrc
+fi
+
 echo "::step::Downloading stable-diffusion model."
 bash scripts/download_sd-v1-4.sh
